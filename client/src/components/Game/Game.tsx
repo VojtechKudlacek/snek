24fd436cfe56d8d64d4bd41781fd456861dcccb2
Vojtěch Kudláček <vjtkudlacek@gmail.com>
2020-10-09 16:17:41 +0200

    ...

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -1,6 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, createRef, ReactNode, RefObject } from 'react';
 
-class Game extends Component {
+interface Properties {}
+
+class Game extends Component<Properties> {
+
+	private canvasRef: RefObject<HTMLCanvasElement>;
+
+	constructor(props: Properties) {
+		super(props);
+		this.canvasRef = createRef();
+	}
+
+	componentDidMount(): void {
+		this
+	}
+
+	render(): ReactNode {
+		return (
+			<canvas ref={this.canvasRef} />
+		);
+	}
 
 }
 
