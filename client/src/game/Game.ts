fb7279a8e9b7b62a2b01d67b213d414e9cee1760
Vojtech Kudlacek <vojtech.kudlacek@socialbakers.com>
2020-10-25 16:37:31 +0100

    Nothin

--- client/src/game/Game.ts
+++ client/src/game/Game.ts
@@ -6,22 +6,34 @@ interface Properties {
 
 class Game {
 
+	private alive: boolean;
 	private renderer: HTMLCanvasElement;
 	private ctx: CanvasRenderingContext2D;
 	private playerStorage: PlayerStorage;
 
 	constructor(props: Properties) {
+		this.alive = false;
 		this.renderer = props.renderer;
 		this.ctx = this.renderer.getContext('2d')!;
 		this.playerStorage = new PlayerStorage();
+		this.loop = this.loop.bind(this);
+	}
+
+	private loop(time: number): void {
+		this.ctx.clearRect(0, 0, 100, 100);
+		if (this.alive) {
+			requestAnimationFrame(this.loop);
+		}
 	}
 
 	public start(): void {
-		this.ctx.clearRect(0, 0, 100, 100);
+		this.alive = true;
 		this.playerStorage.init();
+		requestAnimationFrame(this.loop);
 	}
 
 	public end(): void {
+		this.alive = false;
 		this.playerStorage.destroy();
 	}
 
